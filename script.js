420318acb4bc0bbbf3cd863b610f9cc752376aa2
Schneur <schneuryiftach@gmail.com>
2021-07-08 17:52:18 -0400

    gets users answer and checks if coorect and adds a second if correct

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
+//$(document).ready(function () {
+
+function getRandomIntBetween(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var mathEquation = document.body.querySelector("#mathEquation");
+var firstRandomNumber = 0;
+var secondRandomNumber = 0;
+
+
+var getNewEquation = function () {
+  firstRandomNumber = getRandomIntBetween(1,9);
+  secondRandomNumber = getRandomIntBetween(1,9);
+  mathEquation.innerHTML = firstRandomNumber + " + " + secondRandomNumber;
+};
+
+getNewEquation();
+
+$('#user-input').on('keyup', function () {
+  if (Number($(this).val()) === firstRandomNumber + secondRandomNumber) {
+    console.log("yay");
+    ++seconds;
+  }
+});
+
+
 
 var timerSpan = document.body.querySelector("#timer");
-var seconds = 0;
+var seconds = 10;
 var timer = null;
 
 var startTimer = function () {
   if (!timer) {
     timer = setInterval(function () {
-      timerSpan.innerHTML = ++seconds;
+      timerSpan.innerHTML = --seconds;
     }, 1000);
   }
 };
 
 var restartTimer = function () {
   window.clearInterval(timer);
-  seconds = 0;
+  seconds = 10;
   timer = null;
   timerSpan.innerHTML = seconds;
 };
-    
+  
+//});
